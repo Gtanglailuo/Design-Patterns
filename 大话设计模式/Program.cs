71fb8e08a92bc8b9d1cbfb33828cf3abbbea2094
雷昌政 <3375599273@qq.com>
2023-04-26 11:08:55 +0800

    添加项目文件。

---
+++ 大话设计模式/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 大话设计模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
